9adf2229f4b1501555b2b9d5178060bb48c0ac31
Jane Gotshalk <jane.gotshalk@my.scitexas.edu>
2019-02-15 11:52:24 -0500

    changed i[p address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '00.00.00.00',
+      host: '18.188.162.122',
       username: 'ubuntu',
       privateKey: 'riot-express.pem'
     })
